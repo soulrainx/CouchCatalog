3747564ef241ecb9d74a2032a778faf163fc87aa
Irvin Gonzalez <>
2017-12-05 22:00:04 -0600

    modified gitignore to ignore exported schemas and Production Injector fix

--- app/src/release/java/test/couch/instafit/com/couchcatalog/util/Injector.java
+++ app/src/release/java/test/couch/instafit/com/couchcatalog/util/Injector.java
@@ -1,10 +1,20 @@
 package test.couch.instafit.com.couchcatalog.util;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import test.couch.instafit.com.couchcatalog.data.source.CoachDataSourceMain;
+import test.couch.instafit.com.couchcatalog.data.source.local.CoachDatabase;
+import test.couch.instafit.com.couchcatalog.data.source.local.CoachLocalDataSource;
+import test.couch.instafit.com.couchcatalog.data.source.remote.CoachRemoteDataSource;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class Injector {
     public static CoachDataSourceMain provideCoachDataSourceMain(@NonNull Context context) {
         checkNotNull(context);
         CoachDatabase database = CoachDatabase.getInstance(context);
-        return CoachDataSourceMain.getInstance(CoachLocalDataSource.getInstance(new AppExecutors(),database.coachDao()),
+        return CoachDataSourceMain.getInstance(CoachLocalDataSource.getInstance(new AppExecutors(), database.coachDao()),
                 CoachRemoteDataSource.getInstance());
     }
 }
